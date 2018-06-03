e26f179101474658648be1c296ae3850c7c5438e
C-D-Lewis <Bonsitm@gmail.com>
2018-06-03 19:09:05 +0100

    Add Point contains

--- test-app/src/engine/types.ts
+++ test-app/src/engine/types.ts
@@ -1,5 +1,16 @@
+export class Point {
+
+  constructor(public x: number, public y: number) {}
+
+}
+
 export class Bounds {
 
   constructor(public x: number, public y: number, public width: number, public height: number) {}
+
+  contains(point: Point) {
+    return (point.x > this.x && point.x < (this.x + this.width)) &&
+           (point.y > this.y && point.y < (this.y + this.height));
+  }
 
 }
